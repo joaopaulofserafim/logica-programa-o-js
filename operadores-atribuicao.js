da326a7386f586fc5fba87f181adf138a8f8ca4a
João Paulo <87679335+joaopaulofserafim@users.noreply.github.com>
2024-03-14 14:15:38 +0000

    criando operadores atribuicao

---
+++ operadores-atribuicao.js
@@ -0,0 +1,19 @@
+let numero = 10;
+
+console.log(`O número é inicialmente igual a ${numero}`);
+
+ console.log(`Após reatribuir somando o número 10: ${numero += 10 }`);
+
+ console.log(`Reatribuindo e subtraindo 10: ${numero -= 10}`)
+
+console.log(`Reatribuir e multiplicar 10: ${numero *= 10}`);
+
+console.log(`Reatribuir e dividir 10: ${numero /=10}`);
+
+console.log(`Reatribuindo para o resto da divisão por 10: ${numero %= 10}`);
+
+console.log(`Incrementado o número em 1: ${++ numero}`)
+
+console.log(`Decrementado o número em 1: ${-- numero}`)
+
+console.log(`O número final é igual a: ${numero}`);
