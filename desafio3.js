d9ddcf3d0df457b18af44ed60dc8141e11e81a17
João Paulo <87679335+joaopaulofserafim@users.noreply.github.com>
2024-03-18 21:46:21 +0000

    Desafio 3 concluido?

--- desafio3.js
+++ desafio3.js
@@ -11,27 +11,27 @@ const calC1 = ( temperatura * 9/5) +32,
       calK1 = temperatura - 273.15,
       calK2 = temperatura * 9/5 - 459.67;
 
-      if (atual = `c` || `C` && converter == `f` || `F`) { 
+      if (atual === `c` || `C` && converter === `f` || `F`) { 
         calculo = (temperatura * 9/5 ) + 32 .toFixed(2);
-        window.alert (`A temperatura è ${calculo}} °F`)
+        window.alert (`A temperatura è ${calculo} °F`)
       }
-      else if (atual = `c` || `C` && converter == `k` || `K`) {
+      else if (atual === `c` || `C` && converter === `k` || `K`) {
         calculo = (temperatura + 273.75 .toFixed(2));
         window.alert(`A temperatura é ${calculo} °K`)
       }
-      else if (atual = `f` || `F` && converter == `c` || `C`) {
+      else if (atual === `f` || `F` && converter === `c` || `C`) {
         calculo = (temperatura - 32) * 5/9 .toFixed(2);
         window.alert(`A temperatura é ${calculo} °C`)
       } 
-      else if (atual = `f` || `F` && converter == `k` || `K`){
+      else if (atual === `f` || `F` && converter === `k` || `K`){
         calculo = (temperatura + 459.67) * 5/9 .toFixed(2);
         window.alert(`A temperatura é ${calculo} °K` )
       }
-      else if (atual = `k` || `K` && converter == `c` || `C`){
+      else if (atual === `k` || `K` && converter === `c` || `C`){
         calculo = (temperatura - 273.15.toFixed(2));
         window.alert(`A temperatura é ${calculo} °C`)
       }
-      else if (atual = `k` || `K` && converter == `f` || `F`){
+      else if (atual === `k` || `K` && converter === `f` || `F`){
         calculo = temperatura * 9/5 - 459.67 .toFixed(2); 
         window.alert(`A temperatura é ${calculo}° F`)
       }
