36a009683a25cc0f766491c6a1ee828395368f59
joaopaulofserafim <jpfserafim72@gmail.com>
2024-03-21 10:51:58 -0300

    buttom

--- desafio2.js
+++ desafio2.js
@@ -1,24 +1,29 @@
-const altura = window.prompt ("Digite sua altura"),
-      peso = window.prompt ("Digite seu peso"),
-      imc = peso / (altura ** 2);
+function calculaIMC() {
 
-if ( imc <= 18.5 ) {
-    window.alert (`IMC: ${imc.toFixed(1)}   Abaixo do peso`);
+    const altura = window.prompt("Digite sua altura"),
+        peso = window.prompt("Digite seu peso"),
+        imc = peso / (altura ** 2);
 
-} else if ( imc <= 25 ) {
-    window.alert (` IMC: ${imc.toFixed(1)}   Peso Normal`);
+    if (imc <= 18.5) {
+        window.alert(`IMC: ${imc.toFixed(1)}   Abaixo do peso`);
 
-} else if ( imc <= 30 ) {
-    window.alert (` IMC: ${imc.toFixed(1)}   Sobrepeso`);
+    } else if (imc <= 25) {
+        window.alert(` IMC: ${imc.toFixed(1)}   Peso Normal`);
 
-} else if ( imc <= 35) {
-    window.alert (` IMC: ${imc.toFixed(1)}   Obesidade grau I`);
+    } else if (imc <= 30) {
+        window.alert(` IMC: ${imc.toFixed(1)}   Sobrepeso`);
 
-} else if ( imc <= 40) {
-    window.alert (` IMC: ${imc.toFixed(1)}   Obesidade grau II`);
+    } else if (imc <= 35) {
+        window.alert(` IMC: ${imc.toFixed(1)}   Obesidade grau I`);
 
-} else {
-    window.alert (` IMC: ${imc.toFixed(1)}   Obesidade grau III`);
+    } else if (imc <= 40) {
+        window.alert(` IMC: ${imc.toFixed(1)}   Obesidade grau II`);
+
+    } else {
+        window.alert(` IMC: ${imc.toFixed(1)}   Obesidade grau III`);
+    }
+
 }
+
 
 
--- desafio3.js
+++ desafio3.js
@@ -1,36 +1,41 @@
-const temperatura = parseFloat(window.prompt(`Digite a temperatura:`));
+function converteTemperatura(){
 
-if ( !isNaN(temperatura)) {
+
+  const temperatura = parseFloat(window.prompt(`Digite a temperatura:`));
   
-
-  const
-    atual = window.prompt(`Unidade atual C (Celsius), F (Fahrenheit ) ou K (kelvin) :`).toUpperCase(),
-    converter = window.prompt(`Quer converter para, C (Celsius), F (Fahrenheit ) ou K (kelvin)?`).toUpperCase();
-
-
-  if ((atual === `C` && converter === `F`)) {
-    window.alert(`A temperatura è ${(temperatura * 9 / 5) + 32} °F`)
-  }
-  else if ((atual === `C` && converter === `K`)) {
+  if ( !isNaN(temperatura)) {
     
-    window.alert(`A temperatura é ${(temperatura * 1) + 273} °K`)
+  
+    const
+      atual = window.prompt(`Unidade atual C (Celsius), F (Fahrenheit ) ou K (kelvin) :`).toUpperCase(),
+      converter = window.prompt(`Quer converter para, C (Celsius), F (Fahrenheit ) ou K (kelvin)?`).toUpperCase();
+  
+  
+    if ((atual === `C` && converter === `F`)) {
+      window.alert(`A temperatura è ${(temperatura * 9 / 5) + 32 }°F`)
+    }
+    else if ((atual === `C` && converter === `K`)) {
+      
+      window.alert(`A temperatura é ${(temperatura * 1) + 273} °K`)
+    }
+    else if ((atual === `F` && converter === `C`)) {
+      window.alert(`A temperatura é ${(temperatura - 32) * 5 / 9} °C`)
+    }
+    else if ((atual === `F` && converter === `K`)) {
+      window.alert(`A temperatura é ${(temperatura + 459.67) * 5 / 9} °K`)
+    }
+    else if (atual === `K` && converter === `C`) {
+      window.alert(`A temperatura é ${temperatura - 273.15} °C`)
+    }
+    else if (atual === `K` && converter === `F`) {
+      window.alert(`A temperatura é ${(temperatura * 1) * 9 / 5 - 459.67}° F`)
+    }
+    else {
+      window.alert(`Caracter não existente`)
+    }
+   
+  } else {
+    window.alert(`Caracter não indetificado, digite um número`)
   }
-  else if ((atual === `F` && converter === `C`)) {
-    window.alert(`A temperatura é ${(temperatura - 32) * 5 / 9} °C`)
-  }
-  else if ((atual === `F` && converter === `K`)) {
-    window.alert(`A temperatura é ${(temperatura + 459.67) * 5 / 9} °K`)
-  }
-  else if (atual === `K` && converter === `C`) {
-    window.alert(`A temperatura é ${temperatura - 273.15} °C`)
-  }
-  else if (atual === `K` && converter === `F`) {
-    window.alert(`A temperatura é ${(temperatura * 1) * 9 / 5 - 459.67}° F`)
-  }
-  else {
-    window.alert(`Caracter não existente`)
-  }
- 
-} else {
-  window.alert(`Caracter não indetificado, digite um número`)
+
 }
