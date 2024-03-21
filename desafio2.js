f53c88a3ba05632c386359109c0bcf585cbfa3f5
joaopaulofserafim <jpfserafim72@gmail.com>
2024-03-21 11:53:22 -0300

    Implementações

--- desafio2.js
+++ desafio2.js
@@ -1,28 +1,29 @@
-function calculaIMC() {
+function calculaIMC(peso, altura) {
 
-    const altura = window.prompt("Digite sua altura"),
-        peso = window.prompt("Digite seu peso"),
+    const 
         imc = peso / (altura ** 2);
+        let mensagem; 
 
     if (imc <= 18.5) {
-        window.alert(`IMC: ${imc.toFixed(1)}   Abaixo do peso`);
+       mensagem =`IMC: ${imc.toFixed(1)}   Abaixo do peso`;
 
     } else if (imc <= 25) {
-        window.alert(` IMC: ${imc.toFixed(1)}   Peso Normal`);
+       mensagem =` IMC: ${imc.toFixed(1)}   Peso Normal`;
 
     } else if (imc <= 30) {
-        window.alert(` IMC: ${imc.toFixed(1)}   Sobrepeso`);
+       mensagem =` IMC: ${imc.toFixed(1)}   Sobrepeso`;
 
     } else if (imc <= 35) {
-        window.alert(` IMC: ${imc.toFixed(1)}   Obesidade grau I`);
+       mensagem =` IMC: ${imc.toFixed(1)}   Obesidade grau I`;
 
     } else if (imc <= 40) {
-        window.alert(` IMC: ${imc.toFixed(1)}   Obesidade grau II`);
+       mensagem =` IMC: ${imc.toFixed(1)}   Obesidade grau II`;
 
     } else {
-        window.alert(` IMC: ${imc.toFixed(1)}   Obesidade grau III`);
+       mensagem =` IMC: ${imc.toFixed(1)}   Obesidade grau III`;
     }
 
+    return mensagem;
 }
 
 
