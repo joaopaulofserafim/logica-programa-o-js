969330964035de4251915c7ca96f6c2adb7c1780
João Paulo <87679335+joaopaulofserafim@users.noreply.github.com>
2024-04-02 14:50:05 +0000

    ss

--- desafio3.js
+++ desafio3.js
@@ -1,41 +1,40 @@
-const temperatura = parseFloat(window.prompt( `Digite a temperatura:`)),
-      atual = window.prompt( `Unidade atual C (Celsius), F (Fahrenheit ) ou K (kelvin) :`).toUpperCase(),
-      converter = window.prompt(`Quer converter para, C (Celsius), F (Fahrenheit ) ou K (kelvin)?`).toUpperCase();
+const temperatura = parseFloat(window.prompt(`Digite a temperatura:`));
 
-      
-let calculo = 0;
 
-let   calC1 = ( temperatura * 9/5) +32,
-      calC2 = (temperatura * 1)+ 273,
-      calF1 = (temperatura - 32) * 5/9,
-      calF2 = (temperatura + 459.67) * 5/9 ,
-      calK1 = temperatura - 273.15,
-      calK2 = (temperatura * 1) * 9/5 - 459.67;
+if (!isNaN(temperatura)); {
+  const atual = window.prompt(`Unidade atual C (Celsius), F (Fahrenheit ) ou K (kelvin) :`).toUpperCase(),
+    converter = window.prompt(`Quer converter para, C (Celsius), F (Fahrenheit ) ou K (kelvin)?`).toUpperCase();
 
-      if ( atual == `C` &&  converter == `F`){ 
-        calculo = calC1.toFixed(2);
-        window.alert (`A temperatura è ${calculo} °F`)
-      }
-      else if (atual === `C` && converter ===  `K`) {
-        calculo = (temperatura * 1) + 273;
-        window.alert(`A temperatura é ${calculo} °K`)
-      }
-      else if (atual == `F` &&  converter == `C`) {
-        calculo = calF1 .toFixed(2);
-        window.alert(`A temperatura é ${calculo} °C`)
-      } 
-      else if (atual ==  `F` &&  converter == `K`){
-        calculo = calF2 .toFixed(2);
-        window.alert(`A temperatura é ${calculo} °K` )
-      }
-      else if (atual ==  `K` &&  converter == `C`){
-        calculo = calK1 .toFixed(2);
-        window.alert(`A temperatura é ${calculo} °C`)
-      }
-      else if ( atual == `K` &&  converter == `F`){
-        calculo = calK2 .toFixed(2); 
-        window.alert(`A temperatura é ${calculo}° F`)
-      }
-      else {
-        window.alert(`Erro`)
-      }
+
+
+  if ((atual == `C` && converter == `F`)) {
+    calculo = (temperatura * 9 / 5) + 32 .toFixed(2);
+    window.alert(`A temperatura è ${calculo} °F`)
+  }
+  else if ((atual === `C` && converter === `K`)) {
+    calculo = (temperatura * 1) + 273  .toFixed(2);
+    window.alert(`A temperatura é ${calculo} °K`)
+  }
+  else if ((atual == `F` && converter == `C`)) {
+    calculo = (temperatura - 32) * 5 / 9 .toFixed(2);
+    window.alert(`A temperatura é ${calculo} °C`)
+  }
+  else if ((atual == `F` && converter == `K`)) {
+    calculo = (temperatura + 459.67) * 5 / 9 .toFixed(2);
+    window.alert(`A temperatura é ${calculo} °K`)
+  }
+  else if ((atual == `K` && converter == `C`)) {
+    calculo = (temperatura + 459.67) * 5 / 9 .toFixed(2);
+    window.alert(`A temperatura é ${calculo} °C`)
+  }
+  else if ((atual == `K` && converter == `F`)) {
+    calculo = (temperatura * 1) * 9 / 5 - 459.67.toFixed(2);
+    window.alert(`A temperatura é ${calculo}° F`)
+  }
+  else {
+    window.alert(`Erro`);
+  }
+
+} else {
+  window.alert(`Caracter não reconhecido, digite um número`);
+}
