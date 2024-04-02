fbf6a9b285053e41a202faa07a09b898fd71f052
Anderson Maschio <dnino201@gmail.com>
2024-04-02 11:50:47 -0300

    Deus

--- desafiotriangulo/desafio4.js
+++ desafiotriangulo/desafio4.js
@@ -1,11 +1,15 @@
 let resultado 
 
 function TipoTriangulo (ladoA, LadoB, LadoC) {
-    if (ladoA === LadoB & LadoB === LadoC) {
+
+
+    if (ladoA === LadoB && LadoB === LadoC) {
         return "Equilátero";
     } else if (ladoA === LadoB || ladoA === LadoC || LadoB === LadoC){
         return "Isósceles";
-    } else {
+    } else if ( ladoA != LadoB && ladoA != LadoC && LadoB && LadoC) {
         return "Escaleno";
+    } else {
+        return "Essas medidas não são validas"
     }
 } 
