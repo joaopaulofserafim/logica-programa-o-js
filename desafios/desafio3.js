a4f4fa2c5fbf896784dd96c02dbceb0fc8aaab9c
joaopaulofserafim <jpfserafim72@gmail.com>
2024-04-04 11:55:27 -0300

    estilos

--- desafios/desafio3.js
+++ desafios/desafio3.js
@@ -1,41 +1,43 @@
-function converteTemperatura() {
+function converteTemperatura(valorTemperatura, escalaAtual, escalaConvertida) {
 let temperaturaConvertida;
 
-  const temperatura = parseFloat(window.prompt(`Digite a temperatura:`));
+//   const temperatura = parseFloat(window.prompt(`Digite a temperatura:`));
 
-  if (!isNaN(temperatura)) {
-
-
-    const
-      atual = window.prompt(`Unidade atual C (Celsius), F (Fahrenheit ) ou K (kelvin) :`).toUpperCase(),
-      converter = window.prompt(`Quer converter para, C (Celsius), F (Fahrenheit ) ou K (kelvin)?`).toUpperCase();
+  if (!isNaN(valorTemperatura)) {
 
 
-    if ((atual === `C` && converter === `F`)) {
-    temperaturaConvertida = `A temperatura è ${(temperatura * 9 / 5) + 32}°F`
+   //  const
+   //    atual = window.prompt(`Unidade atual C (Celsius), F (Fahrenheit ) ou K (kelvin) :`).toUpperCase(),
+   //    converter = window.prompt(`Quer converter para, C (Celsius), F (Fahrenheit ) ou K (kelvin)?`).toUpperCase();
+
+
+    if ((valorTemperatura === `C` && escalaAtual === `F`)) {
+    escalaConvertida = `A temperatura è ${(temperatura * 9 / 5) + 32}°F`
     }
-    else if ((atual === `C` && converter === `K`)) {
+    else if ((valorTemperatura === `C` && escalaAtual === `K`)) {
 
-       temperaturaConvertida = `A temperatura é ${(temperatura * 1) + 273} °K`
+       escalaConvertida = `A temperatura é ${(temperatura * 1) + 273} °K`
     }
-    else if ((atual === `F` && converter === `C`)) {
-       temperaturaConvertida = `A temperatura é ${(temperatura - 32) * 5 / 9} °C`
+    else if ((valorTemperatura === `F` && escalaAtual === `C`)) {
+       escalaConvertida = `A temperatura é ${(temperatura - 32) * 5 / 9} °C`
     }
-    else if ((atual === `F` && converter === `K`)) {
-       temperaturaConvertida = `A temperatura é ${(temperatura + 459.67) * 5 / 9} °K`
+    else if ((valorTemperatura === `F` && escalaAtual === `K`)) {
+       escalaConvertida = `A temperatura é ${(temperatura + 459.67) * 5 / 9} °K`
     }
-    else if (atual === `K` && converter === `C`) {
-       temperaturaConvertida = `A temperatura é ${temperatura - 273.15} °C`
+    else if (valorTemperatura === `K` && escalaAtual === `C`) {
+       escalaConvertida = `A temperatura é ${temperatura - 273.15} °C`
     }
-    else if (atual === `K` && converter === `F`) {
-       temperaturaConvertida = `A temperatura é ${(temperatura * 1) * 9 / 5 - 459.67}° F`
+    else if (valorTemperatura === `K` && escalaAtual === `F`) {escalaAtual
+       escalaConvertida = `A temperatura é ${(temperatura * 1) * 9 / 5 - 459.67}° F`
     }
     else {
-       temperaturaConvertida = `Caracter não existente`
+       escalaConvertida = `Caracter não existente`
     }
 
   } else {
-     temperaturaConvertida = `Caracter não indetificado, digite um número`
+     escalaConvertida = `Caracter não indetificado, digite um número`
   }
+
+  return escalaConvertida;
 };
 
