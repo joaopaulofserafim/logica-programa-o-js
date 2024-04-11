42fbc4c8001e82cc5a91b3f6952a25d3f6221807
joaopaulofserafim <jpfserafim72@gmail.com>
2024-04-11 11:53:22 -0300

    melhorando

--- ex 2/ex2.js
+++ ex 2/ex2.js
@@ -1,17 +1,25 @@
-function valorDesconto(atual, clientess) {
-
-    if (clientess === 'Cliente VIP') {
-
-        valorFinal = valor - (atual * 0.5);
-
-    } else if (clientess === "Funcionario") {
-        valorFinal = valor - (atual * 0.1);
-    }
-    else {
-        valorFinal = atual;
-    }
+function compra(){
     
+    let valor = parseFloat(document.getElementById('atual').value);
+    let cliente = document.getElementById('clientess').value;
+    let resultado = valor;
 
-    return valorFinal;
+    if (valor > 0) {
+        if ( cliente === 'Cliente VIP')
+        resultado -= (valor * 0.05);
+       document.querySelector('valorFinal').innerHTML = '<strong>Valor a pagar R$:</strong>'
+
+    }else if ( cliente === 'Funcionario') {
+
+        resultado -= (valor * 0.01);  
+        document.querySelector('valorFinal').innerHTML = '<strong>Valor a pagar R$:</strong>'
+
+    }else if ( cliente === 'Comum') 
+    document.querySelector('valorFinal').innerHTML = '<strong>Valor a pagar R$:</strong>'
+
+    else 
+    document.querySelector('valorFinal').innerHTML = 'Digite o valor:'
 }
+
+
 
