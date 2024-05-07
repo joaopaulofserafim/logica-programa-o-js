cc096c551a815a46b24ac95e708a37439fce046b
joaopaulofserafim <jpfserafim72@gmail.com>
2024-05-07 11:56:51 -0300

    style: mechendo na responsividade

--- geral/navba.js
+++ geral/navba.js
@@ -36,39 +36,39 @@ function criaUlNav(ancoras) {
 
 
 
-// // <a> home 
-// const aHome = criaAncora('/index.html', 'Home')
+// // // <a> home 
+// // const aHome = criaAncora('/index.html', 'Home')
 
-// // <li> do home
-// const liHome = document.createElement('li');
-// liHome.append(aHome);
+// // // <li> do home
+// // const liHome = document.createElement('li');
+// // liHome.append(aHome);
 
-// // <a> ex1
-// const aExercicio1 = criaAncora('../ex1/ex1.html', 'Exerc. 1');
+// // // <a> ex1
+// // const aExercicio1 = criaAncora('../ex1/ex1.html', 'Exerc. 1');
 
-// // document.body.append(aExercicio1);
+// // // document.body.append(aExercicio1);
 
-// // <li> do exercício 1
-// const liExercicio1 = document.createElement('li');
-// liExercicio1.appendChild(aExercicio1);
+// // // <li> do exercício 1
+// // const liExercicio1 = document.createElement('li');
+// // liExercicio1.appendChild(aExercicio1);
 
 
 
-// // <a> do exercício 6
-// const aExercicio6 = criaAncora('../ex6/ex6.html', 'Exerc. 6')
+// // // <a> do exercício 6
+// // const aExercicio6 = criaAncora('../ex6/ex6.html', 'Exerc. 6')
 
-// // <li> do exercício 6
-// const liExercicio6 = document.createElement('li');
-// liExercicio6.appendChild(aExercicio6);
+// // // <li> do exercício 6
+// // const liExercicio6 = document.createElement('li');
+// // liExercicio6.appendChild(aExercicio6);
 
-// // <ul> do menu
-// const ulNav = document.createElement('ul');
-// ulNav.appendChild(liHome, liExercicio1, liExercicio6);
+// // // <ul> do menu
+// // const ulNav = document.createElement('ul');
+// // ulNav.appendChild(liHome, liExercicio1, liExercicio6);
 
 // <nav> do menu
 const nav = document.createElement('nav');
 nav.classList.add('menu-navegacao');
-nav.appendChild(ulNav);
+nav.appendChild(criaUlNav(ancoras));
 
 const spanLogo = document.createElement('span');
 spanLogo.innerHTML = 'Menu & breadcrumbs';
@@ -89,11 +89,12 @@ const header = document.createElement('header');
 header.appendChild(divMenuLogo);
 
 
-document.body.insertBefore(header, document.body.firstChild);
+document.body.insertAdjacentElement('afterbegin', header);
 
 
 const linkNavbar = document.createElement('link')
 linkNavbar.setAttribute('rel', 'stylesheet');
 linkNavbar.setAttribute('href', '../geral/navbar.css');
 
-document.head.appendChild(linkNavbar);
+document.querySelector('[href="/css/responsividade.css"]').insertAdjacentElement('beforebegin', linkNavbar)
+
