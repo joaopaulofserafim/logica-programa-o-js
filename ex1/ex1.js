fc12d254aa827ba41f6581a44eab43202aaf90d6
joaopaulofserafim <jpfserafim72@gmail.com>
2024-05-14 21:59:49 -0300

    refactor

--- ex1/ex1.js
+++ ex1/ex1.js
@@ -1,12 +1,12 @@
-document.querySelector('#btVeri').addEventListener('click', function () {
+document.querySelector('#btV').addEventListener('click', function () {
     const LadoA = parseFloat(document.querySelector('#LadoA').value);
     const LadoB = parseFloat(document.querySelector('#LadoB').value);
     const LadoC = parseFloat(document.querySelector('#LadoC').value);
     const resultado = TipoTriangulo(LadoA, LadoB, LadoC);
-    document.querySelector('#resultado').innerHTML = resultado;
+    document.querySelector('#valorFinal').innerHTML = resultado;
 });
-document.querySelector('#clean').addEventListener('click', function () {
-    document.querySelector('#resultado').innerHTML = " ";
+document.querySelector('#btClean').addEventListener('click', function () {
+    document.querySelector('#valorFinal').innerHTML = " ";
 });
 
 
@@ -15,13 +15,14 @@ function TipoTriangulo(ladoA, LadoB, LadoC) {
 let resultado;
 let erro;
 
-if (isNaN(resultado) || resultado < 0) {
-    window.alert ('Insira um valor válido e maior que 0')
-}if (ladoA === LadoB && LadoB === LadoC) {
+// if (isNaN(resultado) || resultado <= 0) {
+//     window.alert ('Insira um valor válido e maior que 0')
+
+ if (ladoA === LadoB && LadoB === LadoC) {
     return "Equilátero";
 } else if (ladoA === LadoB || ladoA === LadoC || LadoB === LadoC) {
     return "Isósceles";
-} else if (ladoA != LadoB && ladoA != LadoC && LadoB && LadoC) {
+} else if (ladoA !== LadoB && ladoA !== LadoC && LadoB !== LadoC) {
     return "Escaleno";
 } else {
     return erro = "Essas medidas não são validas";
