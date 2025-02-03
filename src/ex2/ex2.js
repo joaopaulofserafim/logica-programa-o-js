7781989df05de7b5585faf29f5c315c29a3656e8
joaopaulofserafim <jpfserafim72@gmail.com>
2025-02-03 11:57:32 -0300

    feat: funcionalidades ex2

--- src/ex2/ex2.js
+++ src/ex2/ex2.js
@@ -1,18 +1,18 @@
 document.querySelector('#btV').addEventListener('click', function () {
-
     const clientess = document.querySelector('#clientess').value;
     let atual = document.querySelector('#valor').value;
     atual = parseFloat(atual);
+
     const desconto = valorFinal(clientess, atual);
     document.querySelector('#valorFinal').innerHTML = desconto;
 });
 
 
 function valorFinal(clientess, atual) {
-
     let valorFinal = parseFloat(atual);
+
     if (isNaN(valorFinal) || valorFinal <= 0) {
-      window.alert('Insira um valor válido e maior que 0')
+      return 'Insira um valor válido e maior que 0';
     } if (clientess === 'VIP') {
         valorFinal -= (atual * 0.05);
     } else if (clientess === 'Funcionario') {
@@ -26,7 +26,7 @@ function valorFinal(clientess, atual) {
 
 document.querySelector('#btClean').addEventListener('click', function () {
     document.querySelector('#valorFinal').innerHTML = " ";
-})    
+});
 
 
 
